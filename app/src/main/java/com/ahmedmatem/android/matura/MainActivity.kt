1dcf8e891505e26e1738247f66a93015fadfb874
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-09-24 14:04:20 +0100

    Add firebase authentication ui library

--- app/src/main/java/com/ahmedmatem/android/matura/MainActivity.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/MainActivity.kt
@@ -2,25 +2,10 @@ package com.ahmedmatem.android.matura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        auth = Firebase.auth
-
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
-        // updateUI(currentUser)
     }
 }
