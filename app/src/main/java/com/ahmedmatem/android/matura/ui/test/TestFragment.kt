47e4dd42192b2865bdf0e969efcef1189b60bbf0
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-09-26 09:30:46 +0100

    Add test modle fragment and view model

--- app/src/main/java/com/ahmedmatem/android/matura/ui/test/TestFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/test/TestFragment.kt
@@ -1,4 +1,28 @@
 package com.ahmedmatem.android.matura.ui.test
 
-class TestsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ahmedmatem.android.matura.databinding.FragmentTestBinding
+
+class TestFragment : Fragment() {
+
+    private lateinit var viewModel: TestViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(TestViewModel::class.java)
+
+        val binding = FragmentTestBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
 }
--- app/src/main/java/com/ahmedmatem/android/matura/ui/test/TestViewModel.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/test/TestViewModel.kt
@@ -1,4 +1,13 @@
 package com.ahmedmatem.android.matura.ui.test
 
-class TestsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TestViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Test Fragment"
+    }
+    val text: LiveData<String> = _text
 }
