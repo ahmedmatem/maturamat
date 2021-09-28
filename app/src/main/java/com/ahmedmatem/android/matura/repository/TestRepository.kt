5b5b43fde58cbcd2bb3faed82a33687b71d2b170
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-09-28 15:43:08 +0100

    Add test refresh functions in repository

--- app/src/main/java/com/ahmedmatem/android/matura/repository/TestRepository.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/repository/TestRepository.kt
@@ -1,5 +1,28 @@
 package com.ahmedmatem.android.matura.repository
 
+import androidx.lifecycle.LiveData
+import com.ahmedmatem.android.matura.network.models.Test
+import com.ahmedmatem.android.matura.network.services.TestApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class TestRepository {
-    
+    private val testApiService = TestApi.retrofitService
+
+//    val tests: LiveData<List<Test>> = database.testDao.getGuestTests()
+    val tests: LiveData<List<Test>>? = null
+
+    suspend fun refreshGuestTestList(uuid: String){
+        withContext(Dispatchers.IO){
+            val tests = testApiService.getAllTestForGuest(uuid).toTypedArray()
+//            database.testDao.insert(*tests)
+        }
+    }
+
+    suspend fun refreshUserTestList(authString: String){
+        withContext(Dispatchers.IO){
+            val tests = testApiService.getAllTestByUser(authString).toTypedArray()
+//            database.testDao.insert(*tests)
+        }
+    }
 }
