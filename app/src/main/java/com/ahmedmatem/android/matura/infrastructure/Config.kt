d6ec47d52240fa4fd5324c4f494c83bddebd3a9d
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-09-29 10:07:41 +0100

    Add database class

--- app/src/main/java/com/ahmedmatem/android/matura/infrastructure/Config.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/infrastructure/Config.kt
@@ -1,3 +1,11 @@
 package com.ahmedmatem.android.matura.infrastructure
 
+/**
+ * API configurations
+ */
 const val BASE_API_URL = "https://matypaapi.azurewebsites.net/"
+
+/**
+ * Database configurations
+ */
+const val DB_NAME = "matura_db"
