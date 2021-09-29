047523d569737680fe14b5ac2ddeeb4d108dfc1d
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-09-29 09:49:48 +0100

    create test DAO

--- app/src/main/java/com/ahmedmatem/android/matura/local/daos/TestDao.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/local/daos/TestDao.kt
@@ -1,8 +1,23 @@
 package com.ahmedmatem.android.matura.local.daos
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.ahmedmatem.android.matura.local.entities.TestEntity
 
 @Dao
 interface TestDao {
+    @Query("SELECT * FROM test_table")
+    fun getAll(): LiveData<List<TestEntity>>
 
+    @Query("SELECT * FROM test_table WHERE is_guest")
+    fun getAllForGuest(): LiveData<List<TestEntity>>
+
+    @Query("SELECT * FROM test_table WHERE NOT is_guest")
+    fun getAllForUser(): LiveData<List<TestEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(vararg tests: TestEntity)
+
+    @Delete
+    suspend fun delete(vararg tests: TestEntity)
 }
