f32dc4f128606b037bc6decefcbb2ce0470edd4d
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-10-01 12:46:26 +0100

    Connect a function to view item on click event

--- app/src/main/java/com/ahmedmatem/android/matura/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/auth/login/LoginFragment.kt
@@ -4,37 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ahmedmatem.android.matura.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    private lateinit var viewModel: LoginViewModel
-    private var _binding: FragmentLoginBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel =
-            ViewModelProvider(this).get(LoginViewModel::class.java)
+        loginViewModel =
+            ViewModelProvider(this, LoginViewModel.Factory(requireContext()))
+                .get(LoginViewModel::class.java)
 
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        binding.viewModel = loginViewModel
 
         return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
--- app/src/main/java/com/ahmedmatem/android/matura/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/auth/login/LoginViewModel.kt
@@ -1,13 +1,28 @@
 package com.ahmedmatem.android.matura.ui.auth.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.content.Context
+import android.view.View
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(context: Context) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
+    /*private val _text = MutableLiveData<String>().apply {
         value = "This is login Fragment"
     }
-    val text: LiveData<String> = _text
+    val text: LiveData<String> = _text*/
+
+    fun login(v: View) {
+        
+    }
+
+    class Factory(private val context: Context) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+                return LoginViewModel(context) as T
+            }
+            throw IllegalArgumentException("Unable to construct LoginViewModel")
+        }
+    }
 }
