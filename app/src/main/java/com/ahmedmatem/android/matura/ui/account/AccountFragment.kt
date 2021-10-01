ceaa5e7f5a991955a1c92119ab6be353a3d27bcc
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-10-01 09:43:40 +0100

    Connect to navigation graphs

--- app/src/main/java/com/ahmedmatem/android/matura/ui/account/AccountFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/account/AccountFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.ahmedmatem.android.matura.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
@@ -33,6 +35,13 @@ class AccountFragment : Fragment() {
 //        viewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+
+        val loginBtn: Button = binding.button2
+        val registerBtn: Button = binding.button3
+
+        loginBtn.setOnClickListener {
+            findNavController().navigate(AccountFragmentDirections.actionNavigationAccountToAuthNavigation())
+        }
 
         return binding.root
     }
---
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/auth/login/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.ahmedmatem.android.matura.ui.auth.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.ahmedmatem.android.matura.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var viewModel: LoginViewModel
+    private var _binding: FragmentLoginBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel =
+            ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        val textView: TextView = binding.textLogin
+        viewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
