f8c07f6ff580b71c2c5bec7ddff7ec5453f7fab6
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-11-01 21:08:28 +0000

    Add EmailConfirmService

---
+++ app/src/main/java/com/ahmedmatem/android/matura/network/services/EmailConfirmService.kt
@@ -0,0 +1,7 @@
+package com.ahmedmatem.android.matura.network.services
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class EmailConfirmService: FirebaseMessagingService() {
+
+}
