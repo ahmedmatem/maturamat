6b03c2d0c07456cc096ec8c3d774d4df71521bfc
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-11-02 22:19:03 +0000

    RegistrationInputValidator refactoring

--- app/src/main/java/com/ahmedmatem/android/matura/ui/account/registration/RegistrationInputValidator.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/account/registration/RegistrationInputValidator.kt
@@ -3,23 +3,22 @@ package com.ahmedmatem.android.matura.ui.account.registration
 import com.ahmedmatem.android.matura.infrastructure.*
 import java.util.regex.Pattern
 
-class RegistrationInputValidator(
-    private val username: String,
-    private val password: String,
-    private val passwordConfirm: String
-) {
+class RegistrationInputValidator {
+    private lateinit var username: String
+    private lateinit var password: String
+    private lateinit var passwordConfirm: String
 
-    init {
-        validateInput()
-    }
+    var errors = Error()
 
-    private var errors = Error()
-
-    fun isValid(): Boolean {
+    fun isValid(un: String, p: String, pc: String): Boolean {
+        validateInput(un, p, pc)
         return errors.value == 0
     }
 
-    private fun validateInput() {
+    private fun validateInput(un: String, p: String, pc: String) {
+        username = un
+        password = p
+        passwordConfirm = pc
         errors.value = 0 // initialize no error value
         validateEmail()
         validatePassword()
@@ -75,33 +74,33 @@ class RegistrationInputValidator(
         }
     }
 
-    private class Error {
-        var value: Int = 0
-
-        fun has(error: Int): Boolean {
-            return (value and error) == error
-        }
-
-        fun add(error: Int) {
-            value = value xor error
-        }
-
-        companion object {
-            const val EMAIL_REQUIRED: Int = 0b0000000000000001 // 1
-            const val EMAIL_INVALID_FORMAT: Int = 0b0000000000000010 // 2
-            const val PASSWORD_REQUIRED: Int = 0b0000000000000100 // 4
-            const val PASSWORD_DIGIT_REQUIRED = 0b0000000000001000 // 8
-            const val PASSWORD_LOWERCASE_REQUIRED: Int = 0b0000000000010000 // 16
-            const val PASSWORD_UPPERCASE_REQUIRED: Int = 0b0000000000100000 // 32
-            const val PASSWORD_NON_ALPHANUMERIC_REQUIRED: Int = 0b0000000001000000 // 64
-            const val PASSWORD_REQUIRED_LENGTH: Int = 0b0000000010000000 // 128
-            const val PASSWORD_CONFIRM_REQUIRED: Int = 0b0000000100000000 // 256
-            const val PASSWORDS_NO_MATCH: Int = 0b0000001000000000 // 512
-        }
-    }
-
     companion object {
         private val EMAIL_REGEX_PATTERN: Pattern =
             Pattern.compile("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    }
+}
+
+class Error {
+    var value: Int = 0
+
+    fun has(error: Int): Boolean {
+        return (value and error) == error
+    }
+
+    fun add(error: Int) {
+        value = value xor error
+    }
+
+    companion object {
+        const val EMAIL_REQUIRED: Int = 0b0000000000000001 // 1
+        const val EMAIL_INVALID_FORMAT: Int = 0b0000000000000010 // 2
+        const val PASSWORD_REQUIRED: Int = 0b0000000000000100 // 4
+        const val PASSWORD_DIGIT_REQUIRED = 0b0000000000001000 // 8
+        const val PASSWORD_LOWERCASE_REQUIRED: Int = 0b0000000000010000 // 16
+        const val PASSWORD_UPPERCASE_REQUIRED: Int = 0b0000000000100000 // 32
+        const val PASSWORD_NON_ALPHANUMERIC_REQUIRED: Int = 0b0000000001000000 // 64
+        const val PASSWORD_REQUIRED_LENGTH: Int = 0b0000000010000000 // 128
+        const val PASSWORD_CONFIRM_REQUIRED: Int = 0b0000000100000000 // 256
+        const val PASSWORDS_NO_MATCH: Int = 0b0000001000000000 // 512
     }
 }
--- app/src/main/java/com/ahmedmatem/android/matura/ui/account/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/account/registration/RegistrationViewModel.kt
@@ -1,12 +1,23 @@
 package com.ahmedmatem.android.matura.ui.account.registration
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import android.content.Context
+import androidx.lifecycle.*
 import com.ahmedmatem.android.matura.base.BaseViewModel
+import com.ahmedmatem.android.matura.base.NavigationCommand
+import com.ahmedmatem.android.matura.local.MaturaDb
+import com.ahmedmatem.android.matura.network.Result
+import com.ahmedmatem.android.matura.network.services.AccountApi
+import com.ahmedmatem.android.matura.repository.AccountRepository
 import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 
-class RegistrationViewModel : BaseViewModel() {
+class RegistrationViewModel(private val context: Context) : BaseViewModel() {
+
+    private val inputValidator by lazy { RegistrationInputValidator() }
+
+    private val _accountRepository by lazy {
+        AccountRepository(MaturaDb.getInstance(context).accountDao, AccountApi.retrofitService)
+    }
 
     // Username input
     val username = MutableLiveData("")
@@ -27,7 +38,8 @@ class RegistrationViewModel : BaseViewModel() {
     private val _passwordConfirmValidationMessage = MutableLiveData("")
     val passwordConfirmValidationMessage: LiveData<String> = _passwordConfirmValidationMessage
     private val _showPasswordConfirmValidationMessage = MutableLiveData(false)
-    val showPasswordConfirmValidationMessage: LiveData<Boolean> = _showPasswordConfirmValidationMessage
+    val showPasswordConfirmValidationMessage: LiveData<Boolean> =
+        _showPasswordConfirmValidationMessage
 
     // button Register
     private val _showRegisterButton = MutableLiveData(true)
@@ -42,16 +54,56 @@ class RegistrationViewModel : BaseViewModel() {
             _showRegisterButton.value = false
             showLoading.value = true
             viewModelScope.launch {
-
+                when (val response = _accountRepository.register(
+                    username.value!!,
+                    password.value!!,
+                    passwordConfirm.value!!,
+                    ""// todo: get FCM token
+                )) {
+                    is Result.Success -> onSuccess()
+                    is Result.GenericError -> onGenericError(response)
+                    is Result.NetworkError -> onNetworkError()
+                }
             }
+        } else {
+            invalidateUi()
         }
     }
 
+    private fun invalidateUi() {
+        invalidateUsernameUi()
+        invalidatePasswordui()
+        invalidatePasswordConfirmUi()
+    }
+
+    private fun invalidateUsernameUi() {
+
+    }
+
     private fun isInputValid(): Boolean {
-        return RegistrationInputValidator(
-            username.value!!,
-            password.value!!,
-            passwordConfirm.value!!
-        ).isValid()
+        return inputValidator.isValid()
+    }
+
+    private fun onSuccess() {
+        TODO("onSuccess for register is not implemented yet.")
+    }
+
+    private fun onGenericError(error: Result.GenericError) {
+        TODO("onGenericError for register is not implemented yet.")
+    }
+
+    private fun onNetworkError() {
+        navigationCommand.value = NavigationCommand
+            .To(RegistrationFragmentDirections.actionRegistrationFragmentToNoConnectionFragment())
+    }
+
+    class Factory(val context: Context) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(RegistrationViewModel::class.java)) {
+                return RegistrationViewModel(context) as T
+            }
+            throw IllegalArgumentException("Unable to construct RegistrationViewModel")
+        }
+
     }
 }
