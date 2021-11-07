245801a4096417099ee6dcd7375623119f48174b
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-11-07 10:58:20 +0000

    Create fragment_password_reset layout

--- app/src/main/java/com/ahmedmatem/android/matura/ui/account/reset/PasswordResetViewModel.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/account/reset/PasswordResetViewModel.kt
@@ -1,7 +1,14 @@
 package com.ahmedmatem.android.matura.ui.account.reset
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ahmedmatem.android.matura.base.BaseViewModel
 
 class PasswordResetViewModel: BaseViewModel() {
 
+    private val _showSendPasswordResetFailMessage = MutableLiveData(false)
+    val showSendPasswordResetFailMessage: LiveData<Boolean> = _showSendPasswordResetFailMessage
+
+    private val _failMessage: MutableLiveData<String> = MutableLiveData()
+    val failMessage: LiveData<String> = _failMessage
 }
