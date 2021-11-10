901c93fef3f1d0c132ef62d6d6b8e7a614956734
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-11-10 18:40:52 +0000

    Implement tap on email confirmed notification

--- app/src/main/java/com/ahmedmatem/android/matura/utils/NotificationUtils.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/utils/NotificationUtils.kt
@@ -1,8 +1,11 @@
 package com.ahmedmatem.android.matura.utils
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.ahmedmatem.android.matura.AccountActivity
 import com.ahmedmatem.android.matura.R
 
 private const val NOTIFICATION_ID = 0
@@ -16,16 +19,25 @@ fun NotificationManager.sendNotification(
     contentTitle: String,
     contentText: String
 ) {
+    // Intent to open an activity that correspondents to the notification
+    val contentIntent = Intent(applicationContext, AccountActivity::class.java)
+    val contentPendingIntent: PendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     val builder = NotificationCompat.Builder(
         applicationContext,
         channelId
     )
-
         .setSmallIcon(R.drawable.ic_check_outline)
         .setContentTitle(contentTitle)
         .setContentText(contentText)
+        .setContentIntent(contentPendingIntent)
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setAutoCancel(true)
 
     notify(NOTIFICATION_ID, builder.build())
 }
