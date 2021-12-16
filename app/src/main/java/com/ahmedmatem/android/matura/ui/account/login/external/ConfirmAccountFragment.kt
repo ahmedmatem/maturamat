437b6b2655f7ddf99d00128b9df1c600af882357
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-12-16 20:47:40 +0000

    Code refactoroing

--- app/src/main/java/com/ahmedmatem/android/matura/ui/account/login/external/ConfirmAccountFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/account/login/external/ConfirmAccountFragment.kt
@@ -1,9 +1,11 @@
 package com.ahmedmatem.android.matura.ui.account.login.external
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.ahmedmatem.android.matura.base.BaseFragment
@@ -25,6 +27,15 @@ class ConfirmAccountFragment : BaseFragment() {
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
+
+        viewModel.loginAttemptResult.observe(viewLifecycleOwner, Observer { success ->
+            if (success) {
+                activity?.apply {
+                    setResult(Activity.RESULT_OK)
+                    finish()
+                }
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/ahmedmatem/android/matura/ui/account/login/external/ConfirmAccountViewModel.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/account/login/external/ConfirmAccountViewModel.kt
@@ -1,18 +1,38 @@
 package com.ahmedmatem.android.matura.ui.account.login.external
 
 import android.content.Context
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 import com.ahmedmatem.android.matura.R
 import com.ahmedmatem.android.matura.base.BaseViewModel
+import com.ahmedmatem.android.matura.base.NavigationCommand
+import com.ahmedmatem.android.matura.local.MaturaDb
+import com.ahmedmatem.android.matura.local.preferences.UserPrefs
+import com.ahmedmatem.android.matura.network.Result
+import com.ahmedmatem.android.matura.network.bgDescription
+import com.ahmedmatem.android.matura.network.models.Token
+import com.ahmedmatem.android.matura.network.models.withPassword
+import com.ahmedmatem.android.matura.network.services.AccountApi
+import com.ahmedmatem.android.matura.repository.AccountRepository
+import com.ahmedmatem.android.matura.ui.account.login.LoginFragmentDirections
+import kotlinx.coroutines.launch
 import java.lang.IllegalArgumentException
 
 class ConfirmAccountViewModel(
     private val context: Context,
     private val args: ConfirmAccountFragmentArgs
 ) : BaseViewModel() {
+    private val _userPrefs: UserPrefs by lazy { UserPrefs(context) }
+
+    private val _accountRepository by lazy {
+        AccountRepository(
+            MaturaDb.getInstance(context).accountDao,
+            AccountApi.retrofitService
+        )
+    }
+
+    private val _loginAttemptResult = MutableLiveData<Boolean>()
+    val loginAttemptResult: LiveData<Boolean>
+        get() = _loginAttemptResult
 
     val confirmAccountText: LiveData<String> = MutableLiveData<String>(
         context.getString(
@@ -25,7 +45,35 @@ class ConfirmAccountViewModel(
     val email: LiveData<String> = MutableLiveData<String>(args.email)
 
     fun confirmAccount() {
+        showLoading.value = true
+        viewModelScope.launch {
+            when (val tokenResponse =
+                _accountRepository.requestToken(email.value!!, password.value!!)) {
+                is Result.Success -> {
+                    login(tokenResponse.data.withPassword(password.value))
+                }
+                is Result.NetworkError -> onNetworkError()
+                is Result.GenericError -> onGenericError(tokenResponse)
+            }
+            showLoading.value = false
+        }
+    }
 
+    private suspend fun login(token: Token, saveInLocalDb: Boolean = true) {
+        if (saveInLocalDb) {
+            _accountRepository.saveToken(token)
+        }
+        _userPrefs.setUser(token.userName, password.value)
+        _loginAttemptResult.value = true
+    }
+
+    private fun onNetworkError() {
+        navigationCommand.value = NavigationCommand
+            .To(ConfirmAccountFragmentDirections.actionConfirmAccountFragmentToNoConnectionFragment())
+    }
+
+    private fun onGenericError(response: Result.GenericError) {
+        showSnackBar.value = response.errorResponse?.bgDescription()
     }
 
     class Factory(private val context: Context, private val args: ConfirmAccountFragmentArgs) :
