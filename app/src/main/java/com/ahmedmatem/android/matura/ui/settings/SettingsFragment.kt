ba09a7471b8d5e1f5e7bbef6244d152cff85a40b
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2022-01-01 15:08:23 +0000

    Add settings xml file

--- app/src/main/java/com/ahmedmatem/android/matura/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/settings/SettingsFragment.kt
@@ -8,12 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.ahmedmatem.android.matura.databinding.FragmentNotificationsBinding
+import com.ahmedmatem.android.matura.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: SettingsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var settingsViewModel: SettingsViewModel
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,14 +24,14 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
+        settingsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
