6f7f7d49439eda475520b634f0f8405bfe17c250
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2022-01-01 20:14:28 +0000

    add settings fragment

--- app/src/main/java/com/ahmedmatem/android/matura/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/settings/SettingsFragment.kt
@@ -1,44 +1,13 @@
 package com.ahmedmatem.android.matura.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.ahmedmatem.android.matura.databinding.FragmentSettingsBinding
+import androidx.preference.PreferenceFragmentCompat
+import com.ahmedmatem.android.matura.R
 
-class SettingsFragment : Fragment() {
-
-    private lateinit var settingsViewModel: SettingsViewModel
-    private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+class SettingsFragment : PreferenceFragmentCompat() {
+    
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
