b30cc7262ec0105177ba2cf46f023aee5009b8a3
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2022-01-04 13:28:47 +0000

    Add Timer util

--- app/src/main/java/com/ahmedmatem/android/matura/utils/TimerUtil.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/utils/TimerUtil.kt
@@ -3,7 +3,7 @@ package com.ahmedmatem.android.matura.utils
 import android.os.CountDownTimer
 
 class Timer private constructor(
-    private var millisInFuture: Long,
+    var millisInFuture: Long,
     private val listener: TimerListener
 ) : CountDownTimer(millisInFuture, COUNT_DOWN_INTERVAL) {
 
@@ -28,7 +28,7 @@ class Timer private constructor(
     fun pause() = cancel()
 
     companion object {
-        private const val COUNT_DOWN_INTERVAL: Long = 1000
+        private const val COUNT_DOWN_INTERVAL: Long = 1000 // 1 sec
 
         fun create(millisInFuture: Long, listener: TimerListener): Timer {
             return  Timer(millisInFuture, listener)
