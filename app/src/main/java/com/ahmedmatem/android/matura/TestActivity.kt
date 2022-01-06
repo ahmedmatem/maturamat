d3b105041edfa42abdb45d811cab40c2efce9620
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2022-01-06 15:08:38 +0000

    Add Test Activity

---
+++ app/src/main/java/com/ahmedmatem/android/matura/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.ahmedmatem.android.matura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
