7fc37672c6515e2b14900b7d442e9e6b1f050e7d
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2022-01-09 16:01:40 +0000

    Create timer view model

--- app/src/main/java/com/ahmedmatem/android/matura/ui/general/CountDownTimerFragment.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/general/CountDownTimerFragment.kt
@@ -5,30 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.ahmedmatem.android.matura.R
+import com.ahmedmatem.android.matura.base.BaseFragment
+import com.ahmedmatem.android.matura.base.BaseViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [CountDownTimerFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CountDownTimerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class CountDownTimerFragment : BaseFragment() {
+    override val viewModel: CountDownTimerViewModel
+        get() = ViewModelProvider(this).get(CountDownTimerViewModel::class.java)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +44,23 @@ class CountDownTimerFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_count_down_timer, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CountDownTimerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CountDownTimerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment CountDownTimerFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            CountDownTimerFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
---
+++ app/src/main/java/com/ahmedmatem/android/matura/ui/general/CountDownTimerViewModel.kt
@@ -0,0 +1,6 @@
+package com.ahmedmatem.android.matura.ui.general
+
+import com.ahmedmatem.android.matura.base.BaseViewModel
+
+class CountDownTimerViewModel: BaseViewModel() {
+}
