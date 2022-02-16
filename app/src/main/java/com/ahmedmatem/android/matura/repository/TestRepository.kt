f2db991127e3212bad8add78e3a3ee4ff5512cdc
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2022-02-16 20:17:34 +0000

    Implement Test repository

--- app/src/main/java/com/ahmedmatem/android/matura/repository/TestRepository.kt
+++ app/src/main/java/com/ahmedmatem/android/matura/repository/TestRepository.kt
@@ -1,16 +1,12 @@
 package com.ahmedmatem.android.matura.repository
 
-import android.content.Context
 import androidx.lifecycle.LiveData
-import com.ahmedmatem.android.matura.local.MaturaDb
 import com.ahmedmatem.android.matura.datasource.local.TestLocalDataSource
 import com.ahmedmatem.android.matura.datasource.remote.TestRemoteDataSource
 import com.ahmedmatem.android.matura.local.preferences.UserPrefs
 import com.ahmedmatem.android.matura.network.models.Test
 import com.ahmedmatem.android.matura.network.models.addUsername
 import com.ahmedmatem.android.matura.network.models.addUuid
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import org.koin.java.KoinJavaComponent.inject
 
 class TestRepository {
@@ -18,52 +14,61 @@ class TestRepository {
     private val remoteDataSource: TestRemoteDataSource by inject(TestRemoteDataSource::class.java)
 
     private val userPrefs: UserPrefs by inject(UserPrefs::class.java)
-    private val username: String? by lazy { userPrefs.getUser()?.username }
+    private val user: UserPrefs.User? by lazy { userPrefs.getUser() }
     private val uuid: String by lazy { userPrefs.getUuid() }
 
     val testList: LiveData<List<Test>> by lazy {
-        username?.let {
-            localDataSource.getAllForUser(username)
+        user?.let {
+            localDataSource.getAllForUser(it.username)
         } ?: run { localDataSource.getAllForGuest(uuid) }
     }
-
-    // Todo: (9/9) Create Account data source and use it here to receive User token
 
     /**
      * RefreshTestList - read all tests from remote server and
      * write them in local database. / Replace in case of insert conflict
      */
     suspend fun refreshTestList() {
-        val testList = remoteDataSource.getTestList()
-        testList?.let {
-            username?.let {
-                // Add username to each test in the list
-                testList.addUsername(it)
-            } ?: run {
-                // Add Uuid to each test in the list
-                testList.addUuid(uuid)
+        var testList: List<Test>? = null
+        user?.let {
+            // In case of User
+            testList = remoteDataSource.getAllForUser(it.token!!)
+            testList?.let { list ->
+                list.addUsername(it.username)
             }
-            // Write tests in local database
-            localDataSource.insert(*testList.toTypedArray())
+        } ?: run {
+            // In case of Guest
+            testList = remoteDataSource.getAllForGuest(uuid)
+            testList?.let { list ->
+                list.addUuid(uuid)
+            }
+        }
+
+        testList?.let {
+            localDataSource.insert(*it.toTypedArray())
         }
     }
 
     /**
-     * RefreshLastTests - read last count number of tests from remote server
-     * and write them in local database.
+     * RefreshLastTest
      */
-    suspend fun refreshLastTests(count: Int = 1) {
-        val testList = remoteDataSource.getLastTests(count)
-        testList?.let {
-            username?.let {
-                // Add username to each test in the list
-                testList.addUsername(it)
-            } ?: run {
-                // Add Uuid to each test in the list
-                testList.addUuid(uuid)
+    suspend fun refreshLastTest() {
+        var testList: List<Test>? = null
+        user?.let {
+            // In case of User
+            testList = remoteDataSource.getLastTestsForUser(it.token!!)
+            testList?.let { list ->
+                list.addUsername(it.username)
             }
-            // Write tests in local database
-            localDataSource.insert(*testList.toTypedArray())
+        } ?: run {
+            // In case of Guest
+            testList = remoteDataSource.getLastTestsForGuest(uuid)
+            testList?.let { list ->
+                list.addUuid(uuid)
+            }
+        }
+
+        testList?.let {
+            localDataSource.insert(*it.toTypedArray())
         }
     }
 }
